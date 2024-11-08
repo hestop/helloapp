c6f5dfba74dc909680e6d3daaa600b9a45483944
Heeseo <j.heeseo0623@gmail.com>
2024-11-08 22:32:05 +0900

    add function component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Component1 from './Component1';
+import Component2 from './Component2';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Component1 />
+        <Component2 />
         <a
           className="App-link"
           href="https://reactjs.org"
